73279bfd9374ee8e8a0a4359a12d90cc95bda00c
Chehan Lasindu <clmallikage001@gmail.com>
2026-03-06 08:14:18 +0530

    MediaFlex-Name-&-Logo-Updated

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,7 +6,6 @@ module.exports = function (app) {
       target: 'http://127.0.0.1:5000',
       changeOrigin: true,
       ws: true,
-      // We don't want to strip /api because the backend routes also start with /api
       pathFilter: '/api',
     })
   );
